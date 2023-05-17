c1b71880cea7cf265f24a141c59049fed5a58b87
RUBENS NETO <121876888+RUBENSNETO0001@users.noreply.github.com>
2023-05-17 16:06:17 -0500

    ur

---
+++ ATIVADADE/lok.js
@@ -0,0 +1,13 @@
+var nome ="gosto?"
+console.log(nome)
+
+var numero = 13.56
+document.write(numero )
+
+var idade = 14
+document.write(", "+ idade)
+
+var name = "ruren"
+document.write(", " + name)
+
+alert("olá meu senhor, como vc esta?")
---
+++ variaveis/tenka.js
@@ -0,0 +1,20 @@
+var nome ="Ruren"
+var email ="reruen@gmail.com"
+var saldo = 0.25
+var idade = 1000000
+var sua_mae = "ola"
+
+document.write(nome ) //valida o codigo
+document.write(", meu nome é " + nome) //valida o codigo + meu nome é
+
+console.log("esse e o " + email)// mostra no console do navegador
+console.log(email)// normal
+
+alert("meu saldo é" + saldo)//vai aparecer a janela  
+alert( saldo)//mormal
+
+//exemplos
+var nome ="thiago"
+console.log("olá, "+ nome)
+
+prompt("qual e seu nome?")// vai na janela do site, serve para pergunta ao usuario 
