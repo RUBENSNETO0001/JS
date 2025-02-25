5e5a57ac8b6d7701d3a288cc6052f756653a4fef
RUBENSNETO0001 <RUBENSNETOMARTINSSUAREZNETO@GMAIL.COM>
2025-02-25 13:51:59 -0300

    organização final

--- dom/explicação/ta.js
+++ dom/explicação/ta.js
@@ -4,14 +4,14 @@ let Pessoa = [
     // Criamos o primeiro objeto da lista, isso torna ele o primeiro indice da LISTA que é 0
     {
         nome: "Fulano",
-        idade: 33, 
-        email: "contato@gmail.com" 
+        idade: 33,
+        email: "contato@gmail.com"
     },
 
     // Criamos o segundo objeto da lista,  isso torna ele o segundo indice da LISTA que é 1
     {
         nome: "JotaJota",
-        idade: 60, 
+        idade: 60,
         email: "jota@contato.outlook.com"
     }
 ]
@@ -23,33 +23,22 @@ console.log(Pessoa[0]) // Acessamos o primeiro Objeto. Indice 0 da lista que é 
 
 console.log(Pessoa[0].nome) // Agora podemos acessar as informações do "Fulano" colocando as propriedades dele após o ponto.
 
-
 // Acessando o "JotaJota" 
 
 console.log(Pessoa[1]) // Acessamos o primeiro Objeto. Indice 0 da lista que é o "JotaJota".
 console.log(Pessoa[1].nome) // Agora podemos acessar as informações do "Fulano" colocando as propriedades dele após o ponto.`
 
+let lista = [
 
+    {
+        filme: "genero",
 
-
-
-    let lista = [
-
-        {
-            filme: "genero", 
-
-            listaFilmes:[
-                {
-                    filme1:'sei la'
-                }
-            ]
-
-        },
-
-        
-
-    ]
-        
-
+        listaFilmes: [
+            {
+                filme1: 'sei la'
+            }
+        ]
+    },
+]
 
 alert(lista[0].listaFilmes[0].filme1)
